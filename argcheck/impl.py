db01e75ed56f6cc2e20b92693d7471e5805c7523
James Boyden <jboy@jboy.me>
2023-08-17 19:20:18 +1000

    Include module `argcheck/arg_or_param.py` into module `argcheck/impl.py`.
    Module `argcheck/arg_or_param.py` was a weird little module; it wasn't imported in any other modules besides `argcheck/impl.py` anyway.

--- argcheck/impl.py
+++ argcheck/impl.py
@@ -1,17 +1,59 @@
 """The main implementation of the function argument checking."""
 
 import functools
+
+from abc import ABC, abstractmethod
 from collections import namedtuple
 from inspect import isclass, signature, Parameter
 from typing import Sequence
 
-from .arg_or_param import _DeclFuncParam, _FuncCallArg
 from .better_repr import _get_repr_that_recreates
 from .checks import _AbstractArgValueCheck, isTypeEqualTo, eachAll
 from .exceptions import (
         _InternalExecutionError, AnnotationCompilationError,
         CallArgBindingRejection, CallArgCheckExecutionError)
 from .import_annotated import Annotated, _AnnotatedAlias
+
+
+class _ObjectWithCtorArgsRepr(ABC):
+    """An ABC with a `__repr__()` method that calls a `_ctor_args()` mothed."""
+    @abstractmethod
+    def _ctor_args(self) -> str:
+        pass
+
+    def __repr__(self) -> str:
+        return "{self.__class__.__name__}({ctor_args})".format(
+                self=self, ctor_args=self._ctor_args())
+
+
+class _DeclFuncParam(_ObjectWithCtorArgsRepr):
+    """Describe a single declared function parameter for exceptions."""
+    def __init__(self, idx, name):
+        self.idx = idx
+        self.name = name
+
+    def _ctor_args(self) -> str:
+        return "idx={self.idx}, name={self.name!r}".format(
+                self=self)
+
+    def __str__(self) -> str:
+        return "[{self.idx}]={self.name!r}".format(
+                self=self)
+
+
+class _FuncCallArg(_ObjectWithCtorArgsRepr):
+    """Describe a single function call argument for exceptions."""
+    def __init__(self, idx_or_kwd, val):
+        self.idx_or_kwd = idx_or_kwd
+        self.val = val
+
+    def _ctor_args(self) -> str:
+        return "idx_or_kwd={self.idx_or_kwd!r}, val={self.val!r}".format(
+                self=self)
+
+    def __str__(self) -> str:
+        return "[{self.idx_or_kwd!r}]={self.val!r}".format(
+                self=self)
 
 
 _DeclParamArgChecksPair = \
