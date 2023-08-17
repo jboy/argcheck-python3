561b3c33ee391c66cc4bbdedd99d026aa1dee830
James Boyden <jboy@jboy.me>
2023-08-17 20:14:36 +1000

    Clean up the imports in module `argcheck/__init__.py`.

--- argcheck/__init__.py
+++ argcheck/__init__.py
@@ -6,6 +6,14 @@ There are many like it; this one is mine.
 Project repo with LICENSE and tests: https://github.com/jboy/argcheck-python3
 """
 
-from .checks import *
-from .exceptions import *
+from .checks import (isTypeEqualTo, isNotEmpty, isMonotonicIncr, isPositive,
+        eachAll)
+
+from .exceptions import (ArgCheckException,
+        AnnotationCompilationError, AnnotationConstructionError,
+        CallArgBindingRejection, CallArgCheckException,
+        CallArgCheckExecutionError, CallArgCheckViolation,
+        CallArgTypeCheckViolation, CallArgValueCheckViolation,
+        CallArgEachCheckViolation)
+
 from .impl import Annotated, Sequence, validate_call
